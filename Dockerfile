c3df726f3a02a4131f782a2c4cf1b63b5030217a
zl1fly <robert@kneedrag.org>
2017-08-06 14:25:16 +1200

    modified:   Dockerfile 	modified:   entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER info@uniq-it-4u.co.nz
 ENV DATA_DIR=/data
 
 RUN apt-get -y update && apt-get -y upgrade && \
-    apt-get -y install postfix 
+    apt-get -y install postfix rsyslog
 
 COPY entrypoint.sh /sbin/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,54 @@ POSTFIX_LOGS=${DATA_DIR}/logs
 POSTFIX_SPOOL=${DATA_DIR}/spool
 
 create_postfix_data_dir() {
-  mkdir -p ${POSTFIX_DATA_DIR}
+  mkdir -p ${DATA_DIR}
 
-  # populate default bind configuration if it does not exist
-  if [ ! -d ${POSTFIX_DATA_DIR}/etc ]; then
-    mv /etc/postfix ${POSTFIX_DATA_DIR}/etc
+  # populate default postfix configuration if it does not exist
+  if [ ! -d ${POSTFIX_DATA_DIR} ]; then
+    mkdir -p ${POSTFIX_DATA_DIR}
+    cd /etc/postfix
+    tar cf - . | (cd ${POSTFIX_DATA_DIR}; tar xf - ) 
+    #mv /etc/postfix ${POSTFIX_DATA_DIR}
   fi
+  cd / 
   rm -rf /etc/postfix
-  ln -sf ${POSTFIX_DATA_DIR}/etc /etc/postfix
-  chmod -R 0775 ${POSTFIX_DATA_DIR}
+  ln -sf ${POSTFIX_DATA_DIR} /etc/postfix
+  #chmod -R 0775 ${POSTFIX_DATA_DIR}
 }
 
-create_postfix_data_dir
+create_postfix_spool_dir() {
 
+  # populate postfix spool if it does not exist
+  if [ ! -d ${POSTFIX_SPOOL} ]; then
+    mkdir -p ${POSTFIX_SPOOL}
+    cd /var/spool/postfix
+    tar cf - . | (cd ${POSTFIX_SPOOL}; tar xf - )
+  fi
+  cd /
+  rm -rf /var/spool/postfix
+  ln -sf ${POSTFIX_SPOOL} /var/spool/postfix
+}
+
+
+create_postfix_log_dir() {
+
+  # populate postfix spool if it does not exist
+  if [ ! -d ${POSTFIX_LOGS} ]; then
+    mkdir -p ${POSTFIX_LOGS}
+    cd /var/log
+    tar cf - . | (cd ${POSTFIX_LOGS}; tar xf - )
+  fi
+  cd /
+  rm -rf /var/log
+  ln -sf ${POSTFIX_SPOOL} /var/log
+}
+
+
+create_postfix_data_dir
+create_postfix_spool_dir
+create_postfix_log_dir
+
+/etc/init.d/rsyslog start
 /etc/init.d/postfix start
+
+/bin/bash

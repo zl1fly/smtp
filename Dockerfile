5fe9b92f65248545d69bc43f73103b597d52c829
zl1fly <robert@kneedrag.org>
2017-08-06 16:32:29 +1200

    Changes to be committed: 	modified:   Dockerfile 	modified:   README 	modified:   entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,24 @@ MAINTAINER info@uniq-it-4u.co.nz
 
 ENV DATA_DIR=/data
 
-RUN apt-get -y update && apt-get -y upgrade && \
-    apt-get -y install postfix rsyslog
+WORKDIR /
 
+RUN apt-get -y update && apt-get -y upgrade && \
+    apt-get -y install postfix rsyslog && \
+    rm /var/lib/apt/lists/*lz4 && \
+    apt-get -o Acquire::GzipIndexes=false update && \
+    apt-get -y install wget perl libnet-ssleay-perl openssl libauthen-pam-perl \ 
+                       libpam-runtime libio-pty-perl python apt-show-versions && \
+    wget -q -O /webmin.deb http://www.webmin.com/download/deb/webmin-current.deb && \
+    dpkg -i /webmin.deb
+    
 COPY entrypoint.sh /sbin/entrypoint.sh
 
 RUN chmod 755 /sbin/entrypoint.sh
 
+RUN echo "root:password" | chpasswd
+
 EXPOSE 25/tcp
+EXPOSE 10000/tcp
 
 ENTRYPOINT ["/sbin/entrypoint.sh"]
-CMD ["/bin/bash"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 POSTFIX_DATA_DIR=${DATA_DIR}/etc
 POSTFIX_LOGS=${DATA_DIR}/logs
 POSTFIX_SPOOL=${DATA_DIR}/spool
+WEBMIN_DATA=${DATA_DIR}/webmin
 
 create_postfix_data_dir() {
   mkdir -p ${DATA_DIR}
@@ -37,7 +38,7 @@ create_postfix_spool_dir() {
 
 create_postfix_log_dir() {
 
-  # populate postfix spool if it does not exist
+  # populate postfix logs if it does not exist
   if [ ! -d ${POSTFIX_LOGS} ]; then
     mkdir -p ${POSTFIX_LOGS}
     cd /var/log
@@ -45,15 +46,32 @@ create_postfix_log_dir() {
   fi
   cd /
   rm -rf /var/log
-  ln -sf ${POSTFIX_SPOOL} /var/log
+  ln -sf ${POSTFIX_LOGS} /var/log
+}
+
+create_webmin_dir() {
+
+  # populate postfix logs if it does not exist
+  if [ ! -d ${WEBMIN_DATA} ]; then
+    mkdir -p ${WEBMIN_DATA}
+    cd /etc/webmin
+    tar cf - . | (cd ${WEBMIN_DATA}; tar xf - )
+  fi
+  cd /
+  rm -rf /etc/webmin
+  ln -sf ${WEBMIN_DATA} /etc/webmin
 }
 
 
 create_postfix_data_dir
 create_postfix_spool_dir
 create_postfix_log_dir
+create_webmin_dir
 
+/etc/init.d/webmin start
 /etc/init.d/rsyslog start
 /etc/init.d/postfix start
 
-/bin/bash
+#/bin/bash
+
+tail -f /var/log/mail.log
